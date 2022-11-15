c702eb0ae402dd2b0ac5cbdb1f17cf1b3b101fdf
林廣學 <tomkhlin@g.chu.edu.tw>
2022-11-15 01:41:11 +0000

    更新環境安裝指令檔

--- LMSweb/install-packages.sh
+++ LMSweb/install-packages.sh
@@ -1,6 +1,13 @@
 #!/bin/zsh
 
-dotnet add package Microsoft.EntityFrameworkCore.Design
-dotnet add package Microsoft.EntityFrameworkCore.Tools
-dotnet add package Microsoft.EntityFrameworkCore.SqlServer
-dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design
+
+if [ -z $1 -a -z $2 ];then
+    echo "請輸入參數！！"
+    exit
+fi
+
+dotnet user-secrets set LMSweb "Server=localhost;Database=LMSmodel;User ID=$1;Password=$2;Encrypt=False"
+dotnet add package Microsoft.EntityFrameworkCore.Design -v 6.0.10
+dotnet add package Microsoft.EntityFrameworkCore.Tools -v 6.0.10
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer -v 6.0.10
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design -v 6.0.10
--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
-if [ -z $1 -a -z $2 ];then
-    echo "請輸入參數！！"
-    exit
-fi
+cp zshrc.txt ~
+mv ~/zshrc.txt ~/.zshrc
 
 dotnet tool install -g dotnet-ef
 dotnet tool install -g Microsoft.Web.LibraryManager.Cli
-dotnet user-secrets set LMSweb "Server=localhost;Database=LMSmodel;User ID=$1;Password=$2"
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+source ~/.zshrc
